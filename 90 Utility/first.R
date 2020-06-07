659f58938788e5b291d1278f875c03265b8606fc
ByeongKeon Kim <skysign@gmail.com>
2020-06-07 12:38:29 +0900

    Adding str_c() example

--- 90 Utility/first.R
+++ 90 Utility/first.R
@@ -1,7 +1,10 @@
 library(tokenizers)
+library(stringr)
 
 juso = "서울 \t	관악구 \n\n	남현동"
 
-tokens = tokenize_words(juso) 
-
+tokens = tokenize_words(juso, simplify = TRUE) 
 tokens
+
+str = str_c(tokens, collapse = " ")
+str
