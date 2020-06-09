85037ede9aa71e6760d6913db6b690f7eef50536
ByeongKeon Kim <skysign@gmail.com>
2020-06-09 19:39:54 +0900

    Comment out print()

--- 30 Analysis/analysis.R
+++ 30 Analysis/analysis.R
@@ -29,8 +29,7 @@ str8 = "서울시|동작구|사당동"   #|으로 나눔
 
 ParentChild_parent <- function(str, parent, child) {
   pattern = paste0(parent, "(?=[\\s\\|\\t]?", child, ")")
-  print(pattern)
-  # match = grep(pattern, str, perl = TRUE)
+  #print(pattern)
   match = str_match(str, pattern)
     
   if (is.na(match) > 0)
@@ -41,8 +40,7 @@ ParentChild_parent <- function(str, parent, child) {
 
 ParentChild_Child <- function(str, parent, child) {
   pattern = paste0("(?<=", parent, "[\\s\\|\\t]?)", child)
-  print(pattern)
-  # match = grep(pattern, str, perl = TRUE)
+  #print(pattern)
   match = str_match(str, pattern)
   
   if (is.na(match) > 0)
@@ -60,12 +58,12 @@ SiGu_Gu <- function(str, parent, child) {
 }
 
 SiGu <- function(str, parent, child) {
-  print(str)
+  #print(str)
   match = SiGu_Si(str, parent, child)
-  print(match)
+  #print(match)
   if (parent == match) {
     match = SiGu_Gu(str, parent, child)
-    print(match)
+    #print(match)
     if (child == match) {
       return(TRUE)
     }
@@ -83,14 +81,14 @@ GuDong_Dong <- function(str, parent, child) {
 }
 
 GuDong <- function(str, parent, child) {
-  print(str)
-  print(parent)
-  print(child)
-  match = GuDong_gu(str, parent, child)
-  print(match)
+  #print(str)
+  #print(parent)
+  #print(child)
+  match = GuDong_Gu(str, parent, child)
+  #print(match)
   if (parent == match) {
     match = GuDong_Dong(str, parent, child)
-    print(match)
+    #print(match)
     if (child == match) {
       return(TRUE)
     }
@@ -107,4 +105,3 @@ SiGu(str1,"서울시","동작구")
 GuDong_Gu(str1,"동작구","사당동")
 GuDong_Dong(str1,"동작구","사당동")
 GuDong(str1,"동작구","사당동")
-# SiGuDong <- fuction(str, )
