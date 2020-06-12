c1b90f031c7c5d10a016c7752570da3b19bec7bf
ByeongKeon Kim <skysign@gmail.com>
2020-06-12 19:34:53 +0900

    Unzip test from package internal file

---
+++ R/analysis.R
@@ -0,0 +1,107 @@
+# look ahead
+# 서울시\s(?=동작)
+# 서울시 동작구 사당동
+# 서울시 동망구 사당동
+# 서울 동작 사당동
+# 
+# look behind
+# (?<=서울시)\s동작
+# 서울시 동작구 사당동
+# 서울시 동망구 사당동
+# 서울 동작 사당동
+
+# 길이가 긴 것 부터, 짧은 순으로
+# 11 서울특별시
+# 11 서울시
+# 11 서울
+
+library(stringr)
+
+# str1 = "서울시 동작구 서울시 동작구 사당동 O"
+str1 = "서울시 동작구 사당동 O"
+str2 = "서울특별시 동작구 사당동 O"
+str3 = "서울 동작구 사당동 O"
+str4 = "서울시 동망구 사당동 X"
+str5 = "서울 동작 사당동 O"
+str6 = "서울시동작구사당동"     #공백이 없음
+str7 = "서울시 동작구 사당동" #tab으로 나눔
+str8 = "서울시|동작구|사당동"   #|으로 나눔
+
+ParentChild_parent <- function(str, parent, child) {
+  pattern = paste0(parent, "(?=[\\s\\|\\t]?", child, ")")
+  #print(pattern)
+  match = str_match(str, pattern)
+    
+  if (is.na(match) > 0)
+    return("")
+    
+  return(match)
+}
+
+ParentChild_Child <- function(str, parent, child) {
+  pattern = paste0("(?<=", parent, "[\\s\\|\\t]?)", child)
+  #print(pattern)
+  match = str_match(str, pattern)
+  
+  if (is.na(match) > 0)
+    return("")
+  
+  return(match)
+}
+
+SiGu_Si <- function(str, parent, child) {
+  return(ParentChild_parent(str, parent, child))
+}
+
+SiGu_Gu <- function(str, parent, child) {
+  return(ParentChild_Child(str, parent, child))
+}
+
+SiGu <- function(str, parent, child) {
+  #print(str)
+  match = SiGu_Si(str, parent, child)
+  #print(match)
+  if (parent == match) {
+    match = SiGu_Gu(str, parent, child)
+    #print(match)
+    if (child == match) {
+      return(TRUE)
+    }
+  }
+  
+  return(FALSE)
+}
+
+GuDong_Gu <- function(str, parent, child) {
+  return(ParentChild_parent(str, parent, child))
+}
+
+GuDong_Dong <- function(str, parent, child) {
+  return(ParentChild_Child(str, parent, child))
+}
+
+GuDong <- function(str, parent, child) {
+  #print(str)
+  #print(parent)
+  #print(child)
+  match = GuDong_Gu(str, parent, child)
+  #print(match)
+  if (parent == match) {
+    match = GuDong_Dong(str, parent, child)
+    #print(match)
+    if (child == match) {
+      return(TRUE)
+    }
+  }
+  
+  return(FALSE)
+}
+
+
+SiGu_Si(str1,"서울시","동작구")
+SiGu_Gu(str1,"서울시","동작구")
+SiGu(str1,"서울시","동작구")
+
+GuDong_Gu(str1,"동작구","사당동")
+GuDong_Dong(str1,"동작구","사당동")
+GuDong(str1,"동작구","사당동")
---
+++ R/onload.R
@@ -0,0 +1,62 @@
+KAAhello.onAttach <- function(libname, pkgname) {
+  ver = 'Korea Address was released in 202004'
+  msg = paste(pkgname, ver)
+  packageStartupMessage(msg)
+}
+
+KAAhello.onLoad <- function(libname, pkgname) {
+  msg = 'Loading begins'
+  packageStartupMessage(msg)
+  
+  library(dplyr)
+  
+  zipFile = system.file("data", "JiBeon_Korea.zip", package = pkgname)
+  csvFile = unz(zipFile, "JiBeon_Korea.csv")
+  
+  df = read.csv(csvFile)
+  
+  df$BeopJeongDongKoDeu = as.character(df$BeopJeongDongKoDeu) # 법정동코드 10자리 , 앞 두자리는 시도, 3~5자리는 구
+  df$SiDoMyeong = as.character(df$SiDoMyeong) #시, 도
+  df$SiGunGuMyeong = as.character(df$SiGunGuMyeong) #구
+  df$BeopJeongEupMyeonDongMyeong = as.character(df$BeopJeongEupMyeonDongMyeong) #동
+  
+  df$JiBeonBonBeon.BeonJi = as.character(df$JiBeonBonBeon.BeonJi) #지번본번
+  df$JiBeonBuBeon.Ho = as.character(df$JiBeonBuBeon.Ho) #지번부번
+  
+  columns = c('BeopJeongDongKoDeu', 'SiDoMyeong', 'SiGunGuMyeong',
+              'BeopJeongEupMyeonDongMyeong')
+  df2 <- df[columns]
+
+  df2['code_root'] = '0'
+  df2['code_Si'] = substr(df2$BeopJeongDongKoDeu, 1, 2) # 법정동 코드에서 시 분리
+  df2['code_Gu'] = substr(df2$BeopJeongDongKoDeu, 3, 5) # 법정동 코드에서 구 분리
+  df2['code_Dong'] = substr(df2$BeopJeongDongKoDeu, 6, 8) # 법정동 코드에서 동 분리
+  
+  siColumns = c('code_root', 'code_Si', 'SiDoMyeong')
+  guColumns = c('code_Si', 'code_Gu', 'SiGunGuMyeong')
+  dongColumns = c('code_Gu', 'code_Dong', 'BeopJeongEupMyeonDongMyeong')
+  
+  dfSi = df2[siColumns]
+  dfGu = df2[guColumns]
+  dfDong = df2[dongColumns]
+  
+  dfSi = unique(dfSi)
+  dfGu = unique(dfGu)
+  dfDong = unique(dfDong)
+  
+  newColumns = c('parent', 'child', 'HaengJeongGuYeok')
+  
+  colnames(dfSi) = newColumns
+  colnames(dfGu) = newColumns
+  colnames(dfDong) = newColumns
+  
+  printf <- function(...) {
+    # print(sprintf(...))
+  }
+  
+  dfsRec = list(dfSi, dfGu, dfDong)
+  fnsRec = list(SiGu, GuDong)
+  
+  msg = 'Loading ends'
+  packageStartupMessage(msg)
+}
