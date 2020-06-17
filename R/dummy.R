96584f8eab406d0a3908f44e1c6959ba5bf9d393
ByeongKeon Kim <skysign@gmail.com>
2020-06-17 14:12:51 +0900

    Check travis is able to commit docs

---
+++ R/dummy.R
@@ -0,0 +1,9 @@
+#' @title dummy function
+#' @description Just to check build status in CI
+#' @details This dummy funtion is only to verify build status in CI(Travis CI and appveyor).
+#' @export
+dummy <- function(juso) {
+  rtn = 'dummy 061700'
+  print(rtn)
+  return(rtn)
+}
