1b99a068d771ab1f1ad20452b9b8071f02f2fd56
ByeongKeon Kim <skysign@gmail.com>
2020-06-17 14:16:18 +0900

    Fix dummy doc

--- R/dummy.R
+++ R/dummy.R
@@ -1,8 +1,9 @@
 #' @title dummy function
 #' @description Just to check build status in CI
 #' @details This dummy funtion is only to verify build status in CI(Travis CI and appveyor).
+#' @return dummy string is returned
 #' @export
-dummy <- function(juso) {
+dummy <- function() {
   rtn = 'dummy 061700'
   print(rtn)
   return(rtn)
