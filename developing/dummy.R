fe653373b76d4f1ec06cb664946fbadf8cfcd626
ByeongKeon Kim <skysign@gmail.com>
2020-06-22 19:37:38 +0900

    Removing dummy from R

--- developing/dummy.R
+++ developing/dummy.R
@@ -1,6 +1,10 @@
+#' @title dummy function
+#' @description Just to check build status in CI
+#' @details This dummy funtion is only to verify build status in CI(Travis CI and appveyor).
+#' @return dummy string is returned
 #' @export
-dummy <- function(juso) {
-  rtn = 'dummy 061600'
+dummy <- function() {
+  rtn = 'dummy 061700'
   print(rtn)
   return(rtn)
 }
